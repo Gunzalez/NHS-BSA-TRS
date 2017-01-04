4007495106f3aaeb18fa8dad76131ec7b6ceeaba
segun.konibire <segun.konibire@valtech.co.uk>
2017-01-04 18:20:06 +0000

    Implements dummy validation for sign up page in JavaScript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,22 +11,64 @@
 // }
 
 $(document).ready(function () {
-  // Use GOV.UK selection-buttons.js to set selected
-  // and focused states for block labels
-  var $blockLabels = $(".block-label input[type='radio'], .block-label input[type='checkbox']");
-  new GOVUK.SelectionButtons($blockLabels); // eslint-disable-line
 
-  // Use GOV.UK shim-links-with-button-role.js to trigger a link styled to look like a button,
-  // with role="button" when the space key is pressed.
-  GOVUK.shimLinksWithButtonRole.init();
+    // Use GOV.UK selection-buttons.js to set selected
+    // and focused states for block labels
+    var $blockLabels = $(".block-label input[type='radio'], .block-label input[type='checkbox']");
+    new GOVUK.SelectionButtons($blockLabels); // eslint-disable-line
 
-  // Show and hide toggled content
-  // Where .block-label uses the data-target attribute
-  // to toggle hidden content
-  var showHideContent = new GOVUK.ShowHideContent();
-  showHideContent.init();
+    // Use GOV.UK shim-links-with-button-role.js to trigger a link styled to look like a button,
+    // with role="button" when the space key is pressed.
+    GOVUK.shimLinksWithButtonRole.init();
 
-  // nhs bsa specific
+    // Show and hide toggled content
+    // Where .block-label uses the data-target attribute
+    // to toggle hidden content
+    var showHideContent = new GOVUK.ShowHideContent();
+    showHideContent.init();
+
+    // nhs bsa specific
+
+    var reportError = function ($obj) {
+        var $errorLayer = $obj.parents('.error-layer');
+        $errorLayer.addClass('error');
+    };
+
+    var clearErrors = function () {
+        $('.error').removeClass('error');
+        $('.error-summary').removeClass('error');
+    };
+
+    var $formsToValidate = $('.form.validate');
+    $formsToValidate.each(function (i, obj) {
+        var $form = $(obj);
+
+        $form.on('submit', function (evt) {
+            evt.preventDefault();
+            clearErrors();
+            $form.data('error', false);
+
+            if($form.attr("id") == "sign-in-form"){
+                var $userId = $('#userID', $form);
+                if($.trim($userId.val()) == ""){
+                    reportError($userId);
+                    $form.data('error', true);
+                }
+
+                var $password = $('#password', $form);
+                if($.trim($password.val()) == ""){
+                    reportError($password);
+                    $form.data('error', true);
+                }
+            }
+
+            if(!$form.data('error')){
+                location.assign($form.attr('action'));
+            } else {
+                $('.error-summary').addClass('error');
+            }
+        })
+    })
 
 
 });
